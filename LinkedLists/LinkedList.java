12fba0eab8ce069c95e2bb7fee870cd3fdcf8ff1
Noel1827 <noel.vargas5@upr.edu>
2021-02-28 00:43:37 -0400

    TILL LINKEDLIST TO ARRAY

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class LinkedList{
     private class Node{
         private int value;
@@ -13,11 +15,93 @@ public class LinkedList{
 
     public void addLast(int item){
         var node = new Node(item);
-        if(first == null)
+        if(isEmpty())
         first = last = node;
         else {
             last.next = node;
             last = node;
         }
+        size++;
     }
+
+    public void addFist(int item){
+        var node = new Node(item);
+
+        if(isEmpty())
+        first = last = node;
+        else{
+            node.next = first;
+            first = node;
+        }
+        size++;
+    }
+
+    private boolean isEmpty(){
+        return first == null;
+    }
+
+    public int indexOf(int item){
+        int index = 0;
+        var current = first;
+        while(current != null){
+            if(current.value == item)return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item){
+       return indexOf(item) != -1;
+    }
+
+    public void deleteFirst(){
+        if(isEmpty())
+        throw new NoSuchElementException();
+
+      if (first == last) {
+        first = last = null;
+    } else {
+        var second = first.next;
+        first.next = null;
+        first = second;
+    }
+    size--;
+    }
+
+    public void removeLast(){
+
+        if (first == last) {
+            first = last = null;
+        } else {
+            var previous = getPrevious(last);
+            last = previous;
+            last.next = null;
+        }
+        size--;
+    }
+
+    private Node getPrevious(Node node){
+        var current = first;
+        while(current != null){
+            if(current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    public int size(){
+        return size;
+    }
+
+public int[] toArray(){
+    int[] array = new int[size];
+    var current = first;
+    var index = 0;
+    while(current != null){
+        array[index++] = current.value;
+        current = current.next;
+    }
+    return array;
+}
 }
--- LinkedLists/Main.java
+++ LinkedLists/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String args[]){
-    LinkedList list = new LinkedList();
-    list.addLast(10);
-    list.addLast(20);
-    list.addLast(30);
+        LinkedList list = new LinkedList();
+       
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
 
-    
+        var array = list.toArray();
+        System.out.println(java.util.Arrays.toString(array));
+      
     }
 }
